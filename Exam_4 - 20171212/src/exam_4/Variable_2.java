3b9a11f87d057ba870262131899e4e167736dc67
Yeon <bsuyeon16@gamil.com>
2017-12-15 13:39:06 +0900

    변수의 초기화

---
+++ Exam_4 - 20171212/src/exam_4/Variable_2.java
@@ -0,0 +1,19 @@
+package exam_4;
+
+public class Variable_2 {
+
+	public static void main(String[] args) {
+		//변수의 초기화
+		boolean b = true; //true, false
+		char c = 'A';
+		String s = "문자열";
+		byte bb = 1;
+		short sh = 2;
+		int gg = 100;
+		long l = 100;
+		float ff = 100.0f;
+		double dd = 100.0;
+		
+	}
+
+}
