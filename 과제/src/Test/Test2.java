3d06186f4404be545fca81e04a83f17d30582b6d
Yeon <bsuyeon16@gamil.com>
2017-12-15 13:40:28 +0900

    삼항연산자

---
+++ 과제/src/Test/Test2.java
@@ -0,0 +1,23 @@
+package Test;
+
+import java.util.Scanner;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		//숫자 하나를 입력을 받아서 삼항 연산자를 사용해서
+		//홀인지 짝인지 판별
+		//짝이면 true	짝입니다.
+		//홀이면 false	홀입니다.
+		
+		Scanner s = new Scanner(System.in);
+		int a = 0;
+		System.out.print("숫자를 입력해 주세요.");
+		a = s.nextInt();
+		String str = (a%2 == 0)?"짝수입니다.":"홀수입니다.";
+		
+		System.out.println(str);
+
+	}
+
+}
