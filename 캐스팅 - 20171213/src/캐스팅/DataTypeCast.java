6140880eae2d89f8c5cf46f4b4515e4cd59e9d54
Yeon <bsuyeon16@gamil.com>
2017-12-15 13:32:38 +0900

    DataType Casting

---
+++ 캐스팅 - 20171213/src/캐스팅/DataTypeCast.java
@@ -0,0 +1,15 @@
+package Ä³½ºÆÃ;
+
+public class DataTypeCast {
+
+	public static void main(String[] args) {
+		
+		System.out.println(7/2);
+		
+		System.out.println(7/(float)2);
+		
+		System.out.println((float)(7/2));
+
+	}
+
+}
