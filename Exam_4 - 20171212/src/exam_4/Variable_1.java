0bbe1e494c2b451fe129d277e6a9c3414d72ba89
Yeon <bsuyeon16@gamil.com>
2017-12-15 13:38:57 +0900

    정적변수, 지역변수, 상수

---
+++ Exam_4 - 20171212/src/exam_4/Variable_1.java
@@ -0,0 +1,16 @@
+package exam_4;
+
+public class Variable_1 {
+	//인스턴스 변수
+	static int a = 10; //정적변수
+	
+	public static void main(String[] args) {
+		//지역변수
+		//int a = 10;
+		//상수
+		final double Pi = 3.14;
+		
+		System.out.println(a);
+	}
+
+}
