ed9ad1d21681982d1318c88fc1666fe32c55007a
Yeon <bsuyeon16@gamil.com>
2017-12-15 13:35:12 +0900

    비트연산

---
+++ 캐스팅 - 20171213/src/캐스팅/Priority.java
@@ -0,0 +1,21 @@
+package 캐스팅;
+
+public class Priority {
+
+	public static void main(String[] args) {
+		
+		System.out.println("3+2*8-4 = "+(3+2*8-4));
+		
+		System.out.println("(3+2)*(8-4) = "+((3+2)*(8-4)));
+		
+		//비트연산		<<  ,  >>
+		//10  -> 1010
+		//0001  10100
+		//10 << 1
+		//10 >> 1
+		System.out.println("10 << 1 = "+(10<<1));
+		System.out.println("10 >> 1 = "+(10>>1));
+
+	}
+
+}
