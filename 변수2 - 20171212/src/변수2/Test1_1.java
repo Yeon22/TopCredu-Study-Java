f8fbe867fff99c2a1241cb869d0f4b921114eae5
Yeon <bsuyeon16@gamil.com>
2017-12-15 13:38:11 +0900

    1일차 과제 3

---
+++ 변수2 - 20171212/src/변수2/Test1_1.java
@@ -0,0 +1,23 @@
+package 변수2;
+
+public class Test1_1 {  
+
+	 public static void main(String[] args) { 
+		
+
+	  String a = 10 + 7 + "";
+	  String b = 8 + "" + 2; 
+	  String c = "" + 5 + 2; 
+
+
+	  System.out.println(a); 
+	  System.out.println(b); 
+	  System.out.println(c); 
+	  
+	  //왼쪽에서 부터 차근차근 연산한다.
+
+	 }
+
+}
+
+
