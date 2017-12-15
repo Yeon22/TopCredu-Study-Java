4ee877447498aca2705eec317e3eb3058d4ab42c
Yeon <bsuyeon16@gamil.com>
2017-12-15 13:34:16 +0900

    javadoc 연습

---
+++ Exam_3 - 20171212/src/ojc/OjcSample.java
@@ -0,0 +1,27 @@
+package ojc;
+
+/**
+ * 자바초보자용 샘플 클래스
+ * <pre>
+ * <b>History;</b>
+ * 		2015.6.1 최초작성
+ * 		2015.12.1 메소드 추가
+ * @author 탑크리에듀
+ *
+ */
+
+public class OjcSample {
+
+	/**
+	 * 메인 메소드
+	 * @param args 메인 메소드 인자
+	 */
+	
+	public static void main(String[] args) {
+		/**
+		 * 출력문
+		 */
+		System.out.println("자바독 주석연습");
+	}
+
+}
