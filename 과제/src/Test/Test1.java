3b8820f974ddf5891c0082244b04fdd112547dcb
Yeon <bsuyeon16@gamil.com>
2017-12-15 13:41:07 +0900

    3일차 문제 첫번째 - 문제 7번 해석

---
+++ 과제/src/Test/Test1.java
@@ -0,0 +1,19 @@
+package Test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		// 2017.12.12일 과제는 변수2 directory에 있음
+		
+		int x = 8;
+		int y = 3;
+		int result = x++ + --y - ++x + --x + y++;
+		// 8 + 2 - 10 + 9 + 2;
+		
+		System.out.println(result);
+
+	}
+
+}
+
