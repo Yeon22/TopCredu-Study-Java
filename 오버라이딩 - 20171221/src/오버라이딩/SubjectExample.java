b865accab02bde4c7c3595d9303f54f6ce578baf
Yeon <bsuyeon16@gamil.com>
2017-12-21 15:13:47 +0900

    오버라이딩 실행 테스트 - 복습

--- 오버라이딩 - 20171221/src/오버라이딩/SubjectExample.java
+++ 오버라이딩 - 20171221/src/오버라이딩/SubjectExample.java
@@ -23,35 +23,35 @@ class MajorSubject extends Subject{
 public class SubjectExample {
 
 	public static void main(String[] args) {
-//		Subject sb = new Subject();
-//		sb.name = "자바";
-//		sb.printInfo();
-//		
-//		MajorSubject msb = new MajorSubject();
-//		msb.targetGrader = "오버라이딩";
-//		msb.printInfo();
-//		
-//		//업캐스팅
-//		Subject ss = new MajorSubject();
-//		ss.name = "자바";
-//		ss.printInfo();
+		Subject sb = new Subject();
+		sb.name = "자바";
+		sb.printInfo();
 		
-//		Subject ss2 = new Subject();
-//		ss2.name = "자바1111";
-//		ss2.printInfo();
-//		
-//		MajorSubject ms = new MajorSubject();
-//		ms.targetGrader = "111";
-//		ms.printInfo();
+		MajorSubject msb = new MajorSubject();
+		msb.targetGrader = "오버라이딩";
+		msb.printInfo();
 		
 		//업캐스팅
-		Subject sb = new MajorSubject();
-		sb.name = "java";
+		Subject ss = new MajorSubject();
+		ss.name = "자바";
+		ss.printInfo();
+		
+		Subject ss2 = new Subject();
+		ss2.name = "자바1111";
+		ss2.printInfo();
+		
+		MajorSubject ms = new MajorSubject();
+		ms.targetGrader = "111";
+		ms.printInfo();
+		
+		//업캐스팅
+		Subject sb2 = new MajorSubject();
+		sb2.name = "java";
 		
 		//다운캐스팅
-		MajorSubject msb = (MajorSubject)sb;
-		msb.targetGrader = "dd";
-		msb.printInfo();
+		MajorSubject msb2 = (MajorSubject)sb2;
+		msb2.targetGrader = "dd";
+		msb2.printInfo();
 
 	}
 
