63ebe928351467dc3e6a217f700e5b212c174e15
Yeon <bsuyeon16@gamil.com>
2017-12-26 16:06:21 +0900

    스윙이벤트 - 외부클래스 20171226

--- 스윙이벤트 - 20171226/src/스윙이벤트/ListenerSample.java
+++ 스윙이벤트 - 20171226/src/스윙이벤트/ListenerSample.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-
+//외부클래스
 class MyActionListener implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
