521b5499a150e5328e3da4e70426f88337aaaf2f
Yeon <bsuyeon16@gamil.com>
2017-12-26 18:12:12 +0900

    스윙 과제 중급 - 못품 20171226

---
+++ 과제/src/과제_20171226/Project_20171226_3.java
@@ -0,0 +1,13 @@
+package 과제_20171226;
+
+public class Project_20171226_3 {
+//	3. ID와 PASSWORD를 입력하고
+//	   로그인하기 버튼을 입력하면 미리 저장된 ID와 PASSWORD가 맞았을때
+//	   로그인 성공을 콘솔에 출력
+//	   틀리면 로그인 실패를 콘솔에 출력
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ 과제/src/과제_20171226/Project_20171226_4.java
@@ -0,0 +1,12 @@
+package 과제_20171226;
+
+public class Project_20171226_4 {
+//	4. 회원가입 화면을 입력후 가입하기 버튼을 누르면 
+//	   콘솔창에 회원정보를 출력
+//	   취소하기 버튼을 누르면 윈도우창 강제종료 해보기
+
+	public static void main(String[] args) {
+
+	}
+
+}
