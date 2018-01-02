c0781d3f41de09515d807bc1a1d68ccca38847e2
Yeon <bsuyeon16@gamil.com>
2018-01-02 15:20:38 +0900

    Iterator로 사용해보기 20180102

--- 벡터 - 20180102/src/벡터/Test1.java
+++ 벡터 - 20180102/src/벡터/Test1.java
@@ -1,5 +1,6 @@
 package 벡터;
 
+import java.util.Iterator;
 import java.util.Vector;
 
 class Book{
@@ -26,11 +27,21 @@ public class Test1 {
 		book.add(new Book("Knight kill", "Murasaki", "Kyobo", 13000));
 		book.add(new Book("papago", "Naver", "Kyobo", 10000));
 		
-		for(int i=0; i<book.size(); i++) {
-			System.out.println("책이름 : "+book.get(i).Title);
-			System.out.println("저자 : "+book.get(i).Author);
-			System.out.println("출판사 : "+book.get(i).Company);
-			System.out.println("가격 : "+book.get(i).Price);
+//		for(int i=0; i<book.size(); i++) {
+//			System.out.println("책이름 : "+book.get(i).Title);
+//			System.out.println("저자 : "+book.get(i).Author);
+//			System.out.println("출판사 : "+book.get(i).Company);
+//			System.out.println("가격 : "+book.get(i).Price);
+//		}
+		
+		// 이터래이터로 출력해보기
+		Iterator<Book> it = book.iterator();
+		while(it.hasNext()) {
+			Book bk = it.next();
+			System.out.println("책이름 : "+bk.Title);
+			System.out.println("저자 : "+bk.Author);
+			System.out.println("출판사 : "+bk.Company);
+			System.out.println("가격 : "+bk.Price);
 		}
 	}
 
