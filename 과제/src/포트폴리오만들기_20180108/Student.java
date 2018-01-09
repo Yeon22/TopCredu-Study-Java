2138b5b9ebd0abd8a3e916d5eff78fcd45915010
Yeon <bsuyeon16@gamil.com>
2018-01-09 16:29:18 +0900

    나의 자바 프로그램 만들기 - 입력, 수정, 삭제 구현 20180109

--- 과제/src/포트폴리오만들기_20180108/Student.java
+++ 과제/src/포트폴리오만들기_20180108/Student.java
@@ -1,70 +1,292 @@
 package 포트폴리오만들기_20180108;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
-public class Student extends JPanel {
+class Info {
+	String id;
+	String department;
+	String name;
+	String iKor;
+	String iEng;
+	String iMath;
+	String iSience;
 	
+	public Info(String id, String department, String name, String iKor, String iEng, String iMath, String iSience) {
+		super();
+		this.id = id;
+		this.department = department;
+		this.name = name;
+		this.iKor = iKor;
+		this.iEng = iEng;
+		this.iMath = iMath;
+		this.iSience = iSience;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+	
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getiKor() {
+		return iKor;
+	}
+	
+	public void setiKor(String iKor) {
+		this.iKor = iKor;
+	}
+	
+	public String getiEng() {
+		return iEng;
+	}
+	
+	public void setiEng(String iEng) {
+		this.iEng = iEng;
+	}
+	
+	public String getiMath() {
+		return iMath;
+	}
+	
+	public void setiMath(String iMath) {
+		this.iMath = iMath;
+	}
+	
+	public String getiSience() {
+		return iMath;
+	}
+	
+	public void setiSience(String iSience) {
+		this.iSience = iSience;
+	}
+}
+
+
+public class Student extends JPanel {
+	JTextField id;
+	JTextField dept;
+	JTextField name;
+	JTextField iKor;
+	JTextField iEng;
+	JTextField iMath;
+	JTextField iSience;
+	JButton insertBtn;
+	JButton updateBtn;
+	JButton deleteBtn;
+	
+	DefaultTableModel model;
+	JTable table;
+	Vector<String> title = new Vector<String>();
+	Vector<Info> m_Vector = new Vector<Info>();
+	Vector<Object> dataVector = new Vector<Object>();
+
 	Student(){
-		JTextField id;
-		JTextField dept;
-		JTextField name;
-		JTextField iKor;
-		JTextField iEng;
-		JTextField iMath;
-		JButton insertBtn;
-		JButton updateBtn;
-		JButton deleteBtn;
-		
 		add(new JLabel("학번 : "));
-		id = new JTextField(20);
+		id = new JTextField(25);
 		add(id);
 		
 		add(new JLabel("학과 : "));
-		dept = new JTextField(20);
+		dept = new JTextField(25);
 		add(dept);
 		
 		add(new JLabel("이름 : "));
-		name = new JTextField(20);
+		name = new JTextField(25);
 		add(name);
 		
 		add(new JLabel("국어 점수 : "));
-		iKor = new JTextField(20);
+		iKor = new JTextField(7);
 		add(iKor);
 		
 		add(new JLabel("영어 점수 : "));
-		iEng = new JTextField(20);
+		iEng = new JTextField(7);
 		add(iEng);
 		
 		add(new JLabel("수학 점수 : "));
-		iMath = new JTextField(20);
+		iMath = new JTextField(7);
 		add(iMath);
 		
-		String colName[] = {"학번", "학과", "이름", "국어", "영어", "수학"};
-		DefaultTableModel model = new DefaultTableModel(colName,0);
+		add(new JLabel("과학 점수 : "));
+		iSience = new JTextField(7);
+		add(iSience);
 		
-		JTable table = new JTable(model);
-		table.setPreferredScrollableViewportSize(new Dimension(250, 200));
+//		String colName[] = {"학번", "학과", "이름", "국어", "영어", "수학"};
+		title.add("학번");
+		title.add("학과");
+		title.add("이름");
+		title.add("국어");
+		title.add("영어");
+		title.add("수학");
+		title.add("과학");
+		
+		model = new DefaultTableModel();
+		
+		table = new JTable(model);
+		table.setPreferredScrollableViewportSize(new Dimension(300, 200));
 		add(new JScrollPane(table));
+		
+		table.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				table = (JTable)e.getComponent();
+				model = (DefaultTableModel)table.getModel();
+				
+				String sid = (String)model.getValueAt(table.getSelectedRow(), 0);
+				id.setText(sid);
+				String sdept = (String)model.getValueAt(table.getSelectedRow(), 1);
+				dept.setText(sdept);
+				String sname = (String)model.getValueAt(table.getSelectedRow(), 2);
+				name.setText(sname);
+				String siKor = (String)model.getValueAt(table.getSelectedRow(), 3);
+				iKor.setText(siKor);
+				String siEng = (String)model.getValueAt(table.getSelectedRow(), 4);
+				iEng.setText(siEng);
+				String siMath = (String)model.getValueAt(table.getSelectedRow(), 5);
+				iMath.setText(siMath);
+				String siSience = (String)model.getValueAt(table.getSelectedRow(), 6);
+				iSience.setText(siSience);
+			}
+		});
 		
 		insertBtn = new JButton("입력");
 		add(insertBtn);
+		insertBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(id.getText().equals("")) {
+					JOptionPane.showConfirmDialog(null, "학번을 입력하세요.");
+				} else if(dept.getText().equals("")) {
+					JOptionPane.showConfirmDialog(null, "학과를 입력하세요.");
+				} else if(name.getText().equals("")) {
+					JOptionPane.showConfirmDialog(null, "이름을 입력하세요.");
+				} else if(iKor.getText().equals("")) {
+					JOptionPane.showConfirmDialog(null, "국어점수를 입력하세요.");
+				} else if(iEng.getText().equals("")) {
+					JOptionPane.showConfirmDialog(null, "영어점수를 입력하세요.");
+				} else if(iMath.getText().equals("")) {
+					JOptionPane.showConfirmDialog(null, "수학점수를 입력하세요.");
+				} else if(iSience.getText().equals("")) {
+					JOptionPane.showConfirmDialog(null, "과학점수를 입력하세요.");
+				} else {
+					Info cInfo = new Info(id.getText(), dept.getText(), name.getText(), iKor.getText(), iEng.getText(), iMath.getText(), iSience.getText());
+					m_Vector.add(cInfo);
+					dataVector.clear();
+					
+					for(int i=0; i<m_Vector.size(); i++) {
+						Info st = m_Vector.get(i);
+						Vector<String> temp = new Vector<String>();
+						temp.addElement(st.getId());
+						temp.addElement(st.getDepartment());
+						temp.addElement(st.getName());
+						temp.addElement(st.getiKor());
+						temp.addElement(st.getiEng());
+						temp.addElement(st.getiMath());
+						temp.addElement(st.getiSience());
+						dataVector.addElement(temp);
+					}
+					
+					model.setDataVector(dataVector, title);
+				}
+			}
+		});
 		
 		updateBtn = new JButton("수정");
 		add(updateBtn);
+		updateBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(JOptionPane.showConfirmDialog(null, "수정하시겠습니까?", "수정", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+					Info st = new Info(id.getText(), dept.getText(), name.getText(), iKor.getText(), iEng.getText(), iMath.getText(), iSience.getText());
+					m_Vector.setElementAt(st, table.getSelectedRow());
+					
+					Vector<String> temp = new Vector<String>();
+					temp.addElement(id.getText());
+					temp.addElement(dept.getText());
+					temp.addElement(name.getText());
+					temp.addElement(iKor.getText());
+					temp.addElement(iEng.getText());
+					temp.addElement(iMath.getText());
+					temp.addElement(iSience.getText());
+					
+					dataVector.setElementAt(temp, table.getSelectedRow());
+					
+					model.setDataVector(dataVector, title);
+				}
+			}
+		});
 		
 		deleteBtn = new JButton("삭제");
 		add(deleteBtn);
+		deleteBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(JOptionPane.showConfirmDialog(null, "삭제하시겠습니까?", "삭제", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+					m_Vector.remove(table.getSelectedRow());
+					dataVector.clear();
+					
+					for(int i=0; i<m_Vector.size(); i++) {
+						Info st = m_Vector.get(i);
+						Vector<String> temp = new Vector<String>();
+						temp.addElement(st.getId());
+						temp.addElement(st.getDepartment());
+						temp.addElement(st.getName());
+						temp.addElement(st.getiKor());
+						temp.addElement(st.getiEng());
+						temp.addElement(st.getiMath());
+						temp.addElement(st.getiSience());
+						dataVector.addElement(temp);
+					}
+					
+					model.setDataVector(dataVector, title);
+				}
+			}
+		});
 		
-		setSize(300, 480);
+		setSize(350, 480);
 		setVisible(true);
 	}
 
