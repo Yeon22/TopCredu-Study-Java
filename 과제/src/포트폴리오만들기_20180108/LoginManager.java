d5d44f455427f1e8a3bab8a4f6d515772b699612
Yeon <bsuyeon16@gamil.com>
2018-01-09 17:43:54 +0900

    로그인 창 만들기 - UI 20180109

---
+++ 과제/src/포트폴리오만들기_20180108/LoginManager.java
@@ -0,0 +1,105 @@
+package 포트폴리오만들기_20180108;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+class MyDialog extends JDialog{
+	MyDialog(){
+		JTextField d_id = new JTextField(12);
+		JTextField d_pw = new JTextField(12);
+		JTextField name = new JTextField(12);
+		JTextField address = new JTextField(12);
+		JButton okBtn;
+		JButton cancelBtn;
+		
+		setTitle("회원가입");
+		setLayout(new FlowLayout());
+		
+		add(new JLabel("아이디 : "));
+		add(d_id);
+		add(new JLabel("비밀번호 : "));
+		add(d_pw);
+		add(new JLabel("이름 : "));
+		add(name);
+		add(new JLabel("주소 : "));
+		add(address);
+		
+		okBtn = new JButton("회원가입하기");
+		add(okBtn);
+		okBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		cancelBtn = new JButton("취소");
+		add(cancelBtn);
+		cancelBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
+		
+		setSize(230, 200);
+	}
+}
+
+public class LoginManager extends JFrame {
+	JTextField id_text;
+	JTextField pw_text;
+	JButton login;
+	JButton member;
+	
+	MyDialog dialog = new MyDialog();
+	
+	LoginManager(){
+		setTitle("로그인화면");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new FlowLayout());
+		
+		JLabel l_id = new JLabel("아이디 입력");
+		id_text = new JTextField(10);
+		add(l_id);
+		add(id_text);
+		
+		JLabel l_pw = new JLabel("비밀번호 입력");
+		pw_text = new JTextField(10);
+		add(l_pw);
+		add(pw_text);
+		
+		login = new JButton("로그인");
+		add(login);
+		login.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		member = new JButton("회원가입");
+		add(member);
+		member.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dialog.setVisible(true);
+			}
+		});
+		
+		setSize(240,150);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new LoginManager();
+	}
+
+}
