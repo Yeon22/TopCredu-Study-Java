92c65cdca3900f8acc88e4143a1b875899d30d66
Yeon <bsuyeon16@gamil.com>
2018-01-18 17:46:02 +0900

    SQL수업 - 테이블 만들기 20180112

---
+++ SQL_20180111_20180118/src/SQL/수업_20180112.java
@@ -0,0 +1,36 @@
+package SQL;
+
+public class 수업_20180112 {
+	
+	/*
+	
+	--테이블만들기
+	create table ddl_test(
+	    no  NUMBER(3),
+	    name VARCHAR2(10),
+	    birth DATE DEFAULT SYSDATE
+	);
+
+
+	--입력하는 명령문
+	insert into ddl_test (no,name) values(1,'홍길동');
+
+	insert into ddl_test values (2,'이순신','18/01/12');
+
+	select * from ddl_test;
+
+
+	--테이블의 내용을 수정
+	update ddl_test set no = 10
+
+	where name = '이순신';
+
+
+	--테이블에서 데이터 삭제
+	delete from ddl_test
+
+	where no =10;
+
+	*/
+
+}
