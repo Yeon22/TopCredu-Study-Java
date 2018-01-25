88be6ad9528cc72eea23c1cd30a88d935fb3c9d7
Yeon <bsuyeon16@gamil.com>
2018-01-25 11:31:03 +0900

    CSS수업 20180123

---
+++ 20180124 - Web_start/src/CSS/수업_20180125/css_6.java
@@ -0,0 +1,25 @@
+package CSS.¼ö¾÷_20180125;
+
+public class css_6 {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Document</title>
+		    <style>
+		        img[src$=png] {border: 3px solid red;}
+		        img[src$=jpg] {border: 3px solid green;}
+		        img[src$=gif] {border: 3px solid blue;}
+		    </style>
+		</head>
+		<body>
+		    <img src="/img/car.png" width="200" height="250">
+		    <img src="/img/car.jpg" width="200" height="250">
+		    <img src="/img/car.gif" width="200" height="250">
+		</body>
+		</html>
+	 */
+
+}
---
+++ 20180124 - Web_start/src/CSS/수업_20180125/css_7.java
@@ -0,0 +1,33 @@
+package CSS.수업_20180125;
+
+public class css_7 {
+	
+	/*
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title>Document</title>
+	    <style>
+	주석부분        #header h1 {color: purple;}
+	주석부분        #section h1 {color: orange;}
+	        #header > h1 {color: aqua;}
+	        #section > h1 {color: blueviolet;}
+	    </style>
+	</head>
+	<body>
+	    <div id="header">
+	        <h1 class="title">메뉴</h1>
+	        <div id="nav">
+	            <h1>네비게이션</h1>
+	        </div>
+	    </div>
+	    <div id="section">
+	        <h1 class="title">본문</h1>
+	        <p>안녕하세요.</p>
+	    </div>
+	</body>
+	</html>
+	*/
+
+}
