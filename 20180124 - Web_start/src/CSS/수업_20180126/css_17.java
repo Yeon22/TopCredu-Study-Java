45bae988c02e46a0062ea4520fd5619c90e3375e
Yeon <bsuyeon16@gamil.com>
2018-01-26 10:26:25 +0900

    CSS수업 - 크기단위 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_17.java
@@ -0,0 +1,41 @@
+package CSS.수업_20180126;
+
+public class css_17 {
+	
+	/*
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title>크기단위</title>
+	    <style>
+	        
+	주석시작      크기단위    % 백분율 단위
+	        	px         픽셀
+	        	em         배수 단위
+	주석끝
+	        
+	주석시작
+	        p:nth-child(1) {}
+	        p:nth-child(2) {font-size: 1.0em;}
+	        p:nth-child(3) {font-size: 1.5em;}
+	        p:nth-child(4) {font-size: 2em;}
+	주석끝
+
+	        p:nth-child(1) {}
+	        p:nth-child(2) {font-size: 16px;}
+	        p:nth-child(3) {font-size: 24px;}
+	        p:nth-child(4) {font-size: 32px;}
+	        
+	    </style>
+	</head>
+	<body>
+	    <p>안녕하세요.</p>
+	    <p>안녕하세요.</p>
+	    <p>안녕하세요.</p>
+	    <p>안녕하세요.</p>
+	</body>
+	</html>
+	*/
+
+}
