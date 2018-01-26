07b706426c3056f999771cd35f282d9ca740412b
Yeon <bsuyeon16@gamil.com>
2018-01-26 10:28:10 +0900

    CSS수업 - 가시속성(visible,hidden,collapse) 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_21.java
@@ -0,0 +1,36 @@
+package CSS.수업_20180126;
+
+public class css_21 {
+	
+	/*
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title>가시속성</title>
+	    <style>
+	        
+	주석시작   visible    태그를 보이게 만듭니다.
+	        hidden     태그를 보이지 않게 만듭니다.
+	        collapse   table 태그를 보이지 않게 만듭니다.  
+	주석끝
+	        
+	        #box {
+	            visibility: hidden;
+	        }
+	        
+	    </style>
+	</head>
+	<body>
+	   
+	    <span>===================</span>
+	    <div id="box">
+	        안녕하세요.
+	    </div>
+	    <span>===================</span>
+	            
+	</body>
+	</html>
+	*/
+
+}
