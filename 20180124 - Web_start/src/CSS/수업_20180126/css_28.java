c9aeba8be67b5e68f4a9dac07e79d2f96b4e3135
Yeon <bsuyeon16@gamil.com>
2018-01-26 13:08:35 +0900

    CSS수업 - font속성들 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_28.java
@@ -0,0 +1,34 @@
+package CSS.수업_20180126;
+
+public class css_28 {
+	
+	/*
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title>Document</title>
+	    <style>
+	주석시작
+	        .font_arial {font-family: Arial;}
+	        .font_roman {font-family: 'Times New Roman';}
+	        
+	        .font_arial {font-family: sans-serif;}
+	        .font_roman {font-family: serif;}
+	주석끝
+	        .font_big {font-size: 2em;}
+	        .font_italic {font-style: italic;}
+	        .font_bold {font-weight: bold;}
+	    </style>
+	</head>
+	<body>
+	주석시작
+	    <h1 class="font_arial">Hello</h1>
+	    <p class="font_roman">Hello</p>
+	주석끝
+	    <p class="font_big font_italic font_bold">Gracias</p>
+	</body>
+	</html>
+	*/
+
+}
