5adf0523be1321d7114bb223328dea3106583e15
Yeon <bsuyeon16@gamil.com>
2018-01-26 11:17:25 +0900

    CSS수업 - opacity(투명도)속성 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_22.java
@@ -0,0 +1,38 @@
+package CSS.수업_20180126;
+
+public class css_22 {
+	
+	/*
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title>Document</title>
+	    <style>
+	        
+	주석시작      
+			opacity 속성
+	      	투명도
+	      	0 ~ 1사이의 숫자를 입력한다. 0은 투명, 1은 불투명
+	        
+	주석끝
+	        
+	        #box{
+	            background-color: black;
+	            color: white;
+	            opacity: 0;
+	        }
+	        
+	    </style>
+	</head>
+	<body>
+	    <span>=====================</span>
+	    <div id="box">
+	        투명해져라! 익스펙토페트로눔
+	    </div>
+	    <span>=====================</span>
+	</body>
+	</html>
+	*/
+
+}
