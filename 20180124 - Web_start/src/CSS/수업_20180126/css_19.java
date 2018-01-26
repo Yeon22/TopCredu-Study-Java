d0bbd9758889c2fde38fa54af02395e1cab171aa
Yeon <bsuyeon16@gamil.com>
2018-01-26 10:27:08 +0900

    CSS수업 - url 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_19.java
@@ -0,0 +1,26 @@
+package CSS.수업_20180126;
+
+public class css_19 {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>url</title>
+		    <style>
+		        
+		        body {
+		            background-image: url('img/car.jpg');
+		        }
+		        
+		    </style>
+		</head>
+		<body>
+		    <h1>프로그래밍 재밌다.</h1>
+		    <p>자바랑 DB연결하는거 어렵다. 테이블을 다시 만들어야 한다.....</p>
+		</body>
+		</html>
+	 */
+
+}
