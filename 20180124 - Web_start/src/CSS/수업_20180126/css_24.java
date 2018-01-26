a30f2b27153cbc7024a7c65c94e0ffd30d972fa9
Yeon <bsuyeon16@gamil.com>
2018-01-26 11:18:11 +0900

    CSS수업 - 박스사이징 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_24.java
@@ -0,0 +1,40 @@
+package CSS.수업_20180126;
+
+public class css_24 {
+	
+	/*
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title>Document</title>
+	    <style>
+	주석시작      
+	        box-sizing 속성
+	        width 속성과 height 속성은 글자를 감싸는 영역의 크기를 지정하는 속성
+	주석끝
+	        div{
+	            margin: 10px; padding: 10px;
+	            width: 100px; height: 100px;
+	            border: 10px solid black;
+	        }
+	        
+	        div:first-child{
+	            background: red;
+	            box-sizing: content-box;
+	        }
+	        
+	        div:last-child{
+	            background: orange;
+	            box-sizing: border-box;
+	        }
+	    </style>
+	</head>
+	<body>
+	    <div></div>
+	    <div></div>
+	</body>
+	</html>
+	*/
+
+}
