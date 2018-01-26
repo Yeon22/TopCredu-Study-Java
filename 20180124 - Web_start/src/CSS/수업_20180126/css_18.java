177d70a16bd712e256f8103b3fa5e74c0fad46e6
Yeon <bsuyeon16@gamil.com>
2018-01-26 10:26:48 +0900

    CSS수업 - 가시속성 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_18.java
@@ -0,0 +1,31 @@
+package CSS.수업_20180126;
+
+public class css_18 {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>색깔</title>
+		    <style>
+		        
+		        h1{background-color: rebeccapurple;}
+		        h2{background-color: orange;}
+		        h3{background-color: bisque;}
+		        h4{background-color: rgb(0,255,0);}
+		        h5{background-color: #0094FF;}
+		        
+		    </style>
+		</head>
+		<body>
+		    <h1>글을 쓰세요.</h1>
+		    <h2>글을 쓰세요.</h2>
+		    <h3>글을 쓰세요.</h3>
+		    <h4>글을 쓰세요.</h4>
+		    <h5>글을 쓰세요.</h5>
+		</body>
+		</html>
+	 */
+
+}
