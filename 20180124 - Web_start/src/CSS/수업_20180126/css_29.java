7c86fcdefe0e05a2be2b1ea350e041a6aa0ef58e
Yeon <bsuyeon16@gamil.com>
2018-01-26 13:09:19 +0900

    CSS수업 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_29.java
@@ -0,0 +1,37 @@
+package CSS.¼ö¾÷_20180126;
+
+public class css_29 {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Document</title>
+		    <style>
+		        .font_big {font-size: 2em;}
+		        .font_italic {font-style: italic;}
+		        .font_bold {font-weight: bold;}
+		        .font_center {text-align: center;}
+		        .button{
+		            width: 150px;
+		            height: 70px;
+		            background-color: skyblue;
+		            border-radius: 30px;
+		            box-shadow: 5px 5px 5px gray;
+		        }
+		        .button > a {
+		            display: block;
+		            padding: 10px 0px;
+		        }
+		    </style>
+		</head>
+		<body>
+		    <div class="button">
+		        <a href="#" class="font_big font_italic font_bold font_center">Click</a>
+		    </div>
+		</body>
+		</html>
+	 */
+
+}
---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_30.java
@@ -0,0 +1,43 @@
+package CSS.¼ö¾÷_20180126;
+
+public class css_30 {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Document</title>
+		    <style>
+		        
+		        .font_center {text-align: center;}
+		        
+		        span {
+		            background-color: red;
+		        }
+		        
+		        p{
+		            background-color: aquamarine;
+		        }
+		        
+		        a {text-decoration: none;}
+		        
+		        li {list-style: none;}
+		        
+		    </style>
+		</head>
+		<body>
+		   
+		    <span class="font_center">Hello</span>
+		    <p class="font_center">Picante</p>
+		    <a href="www.naver.com">Naver</a>
+		    
+		    <ul>
+		        <li>Gachedong</li>
+		    </ul>
+		    
+		</body>
+		</html>
+	 */
+
+}
