7b223ea5c5f550ed26395572b04ff6e9c3e46e87
Yeon <bsuyeon16@gamil.com>
2018-01-26 13:08:08 +0900

    CSS수업 - font-size 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_27.java
@@ -0,0 +1,29 @@
+package CSS.수업_20180126;
+
+public class css_27 {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>폰트</title>
+		    <style>
+		        .a {font-size: 32px;}
+		        .b {font-size: 2em;}
+		        .c {font-size: large;}
+		        .d {font-size: small;}
+		    </style>
+		</head>
+		<body>
+		    <h1>Hi, Hello, Hola</h1>
+		    <p class="a">H</p>
+		    <p class="b">E</p>
+		    <p class="c">L</p>
+		    <p class="d">L</p>
+		    <p class="e">O</p>
+		</body>
+		</html>
+	 */
+
+}
