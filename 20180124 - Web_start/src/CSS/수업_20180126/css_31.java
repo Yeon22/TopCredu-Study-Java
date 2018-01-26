e14c08d15c7d351af60ed59a1b580fbcf0e54e57
Yeon <bsuyeon16@gamil.com>
2018-01-26 14:18:25 +0900

    CSS수업 - 박스 움직이기 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_31.java
@@ -0,0 +1,62 @@
+package CSS.수업_20180126;
+
+public class css_31 {
+	
+	/*
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title>Position</title>
+	    <style>
+	        
+	주석시작      
+	        static     태그가 위에서 아래로 순서대로 배치됩니다.
+	        relative   초기 위치 상태에서 상하좌우로 위치를 이동합니다.
+	        absolute   절대 위치 좌표를 설정합니다.
+	        fixed      화면을 기준으로 절대 위치 좌표를 설정합니다.
+	주석끝
+	        body > div {
+	            position: relative;
+	            width: 400px; height: 100px;
+	            border: 3px solid black;
+	            overflow: hidden;
+	        }
+	        
+	        .box{
+	            width: 100px; height: 100px;
+	            position: absolute;
+	            background-color: aqua;
+	            left: 10px; top: 10px;
+	            z-index: 100;
+	        }
+	        
+	        .box2{
+	            width: 100px; height: 100px;
+	            position: absolute;
+	            background-color: red;
+	            left: 50px; top: 50px;
+	            z-index: 10;
+	        }
+	        
+	        .box3{
+	            width: 100px; height: 100px;
+	            position: absolute;
+	            background-color: rebeccapurple;
+	            left: 90px; top: 90px;
+	            z-index: 1;
+	        }
+	        
+	    </style>
+	</head>
+	<body>
+	    <div>
+	        <div class="box"></div>
+	        <div class="box2"></div>
+	        <div class="box3"></div>
+	    </div>
+	</body>
+	</html>
+	*/
+
+}
---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_32.java
@@ -0,0 +1,27 @@
+package CSS.수업_20180126;
+
+public class css_32 {
+	
+	/*
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title>float</title>
+	    <style>
+	        .box{
+	            width: 100px; height: 100px;
+	            background-color: red;
+	            float: left;
+	        }
+	    </style>
+	</head>
+	<body>
+	    <div class="box">1</div> <!--기본 div만들었을 때 상태가 static-->
+	    <div class="box">2</div>
+	    <div class="box">3</div>
+	</body>
+	</html>
+	*/
+
+}
