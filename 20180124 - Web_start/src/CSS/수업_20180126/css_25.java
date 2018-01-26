30c101b3cdb7f5bc91fdd9d041749bd07d7e7f39
Yeon <bsuyeon16@gamil.com>
2018-01-26 11:18:45 +0900

    CSS속성 - border속성 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_25.java
@@ -0,0 +1,31 @@
+package CSS.수업_20180126;
+
+public class css_25 {
+	
+	/*
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title>Document</title>
+	    <style>
+	        .box{
+	주석시작
+	            border-width: thick;
+	            border-style: dashed;
+	            border-color: black;
+	주석끝
+	            border: 10px dashed black;
+	            border-radius: 20px;
+	        }
+	    </style>
+	</head>
+	<body>
+	    <div class="box">
+	        <h1>그라시아스</h1>
+	    </div>
+	</body>
+	</html>
+	*/
+
+}
