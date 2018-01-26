a4f375eebdcf2d156fbdf0c6fef6783bea4afd98
Yeon <bsuyeon16@gamil.com>
2018-01-26 10:27:37 +0900

    CSS수업 - display속성 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_20.java
@@ -0,0 +1,43 @@
+package CSS.수업_20180126;
+
+public class css_20 {
+	
+	/*
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title>Document</title>
+	    <style>
+	    
+	주석시작   display 속성
+	        display 속성은 태그의 영역 표현 방식을 지정하는 속성이다.
+	       
+	       display : none     태그를 화면에서 보이지 않게 만든다.
+	       display : block    태그를 block형식으로 지정한다.
+	       display : inline   태그를 inline형식을 지정합니다.
+	       display : inline-block     태그를 inline-block 형식으로 지정합니다.   주석끝
+	        
+	주석시작   block 키워드를 사용하면 태그가 한 줄을 다 차지한다.
+	        inline 키워드 또는 inline-block 키워드를 사용하면 한 줄에 들어간다. 주석끝
+	        
+	        #box {
+	            display: inline-block;
+	            background-color: aqua;
+	            width: 300px; height: 50px; 
+	            margin: 10px;
+	        }
+	        
+	    </style>
+	</head>
+	<body>
+	    <span> Dummy </span>
+	    <div id="box">
+	        <span>안녕하세요.</span>
+	    </div>
+	    <span> Dummy </span>
+	</body>
+	</html>
+	*/
+
+}
