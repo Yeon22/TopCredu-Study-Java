b2efe95dd536401aae9dbfaa29b77fc68fd59679
Yeon <bsuyeon16@gamil.com>
2018-01-26 11:17:50 +0900

    CSS수업 - padding, margin 20180126

---
+++ 20180124 - Web_start/src/CSS/수업_20180126/css_23.java
@@ -0,0 +1,32 @@
+package CSS.수업_20180126;
+
+public class css_23 {
+	
+	/*
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <title>Document</title>
+	    <style>
+	        div{
+	            width: 100px; height: 100px;
+	            background-color: red;
+	            
+	            border: 20px solid black;
+	주석부분           위-아래  왼쪽-오른쪽    
+	주석부분    	  	padding: 10px 30px; 
+	주석부분     	     위 오른쪽 아래 왼쪽    
+	            padding: 10px 0px 0px 10px;
+	            margin: 0 30px;
+	        }
+	    
+	    </style>
+	</head>
+	<body>
+	    <div>Gracias, spice, hi, programming, java, C#, C, 프로그래밍</div>
+	</body>
+	</html>
+	*/
+
+}
