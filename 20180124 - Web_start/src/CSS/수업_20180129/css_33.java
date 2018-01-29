bb7e2365742c5c2e348232e7c22c363c17f0d744
Yeon <bsuyeon16@gamil.com>
2018-01-29 14:22:25 +0900

    CSS수업 - div 연습

---
+++ 20180124 - Web_start/src/CSS/수업_20180129/css_33.java
@@ -0,0 +1,81 @@
+package CSS.¼ö¾÷_20180129;
+
+public class css_33 {
+	
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Document</title>
+		    <style>
+		        #box1{
+		            width: 100px;
+		            height: 100px;
+		            border: 1px solid black;
+		            padding: 10px;
+		            margin: 10px;
+		        }
+		        
+		        #box1 > img {
+		            width: 100px;
+		            height: 100px;
+		        }
+		        
+		        #box2{
+		            width: 100px;
+		            height: 100px;
+		            background-color: bisque;
+		            border: 5px dotted red;
+		            box-shadow: 10px 10px 30px black;
+		        }
+		        
+		        #box2 > img {
+		            width: 100px;
+		            height: 100px;
+		            filter: grayscale(100%);
+		        }
+		        
+		        #box3{
+		            width: 200px;
+		            height: 200px;
+		            background-color: gold;
+		            border-radius: 100px 100px 100px 100px;
+		        }
+		        
+		        #box3 > img {
+		            width: 200px;
+		            height: 200px;
+		            border-radius: 50%;
+		        }
+		        
+		        #box4{
+		            width: 300px;
+		            height: 300px;
+		            background: -webkit-linear-gradient(top, #a7cfdf 0%,#23538a 100%); 
+		        }
+		        
+		    </style>
+		</head>
+		<body>
+		    <div id="box1">
+		        <img src="img/Desert.jpg">
+		    </div>
+		    
+		    <div id="box2">
+		        <img src="img/car.jpg">
+		    </div>
+		    
+		    <div id="box3">
+		        <img src="img/coffee.jpg">
+		    </div>
+		    
+		    <div id="box4">
+		        box4
+		    </div>
+		</body>
+		</html>
+	 */
+
+}
