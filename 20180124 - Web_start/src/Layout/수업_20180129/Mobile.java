d14d03570dd9f3eb56ad6d3750f121bfdd51c2b4
Yeon <bsuyeon16@gamil.com>
2018-01-29 12:21:19 +0900

    레이아웃 - 모바일 20180129

---
+++ 20180124 - Web_start/src/Layout/수업_20180129/Mobile.java
@@ -0,0 +1,107 @@
+package Layout.수업_20180129;
+
+public class Mobile {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>모바일 레이아웃</title>
+		    <!--    viewport 사용    -->
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		    
+		    <style>
+		        * {padding: 0; margin; 0;}
+		        header{
+		            height: 45px;
+		            background-image: url(img/header_background.png);
+		            	자손 위치
+		            position: relative;
+		            text-align: center;
+		            color: white;
+		        }
+		        
+		        .left{
+		            width: 62px;
+		            height: 32px;
+		            text-indent: -99999px;
+		            position: absolute;
+		            background-image: url(img/header_left_button.png);
+		            left: 3px;
+		            top: 5px;
+		        }
+		        
+		         .right{
+		            width: 32px;
+		            height: 32px;
+		            text-indent: -99999px;
+		            position: absolute;
+		            background-image: url(img/header_right_button.png);
+		            right: 3px;
+		            top: 5px;
+		        }
+		        
+		        header h2{
+		            padding-top: 5px;
+		        }
+		        
+		        #gnb{
+		            overflow: hidden;
+		            border-bottom: 1px solid black;
+		            background-color: darkred;
+		        }
+		        
+		        #gnb > div > a{
+		            float: left;
+		            width: 25%;
+		            height: 35px;
+		            line-height: 35px;
+		            text-align: center;
+		            color: white;
+		            text-decoration: none;
+		        }
+		        
+		        footer{
+		            width: 100%;
+		            background-color: black;
+		            color: white;
+		            text-align: center;
+		            position: fixed;
+		            bottom: 0;
+		        }
+		        
+		    </style>
+		    
+		</head>
+		<body>
+		    <div id="wrap">
+		        <header>
+		            <a class="left" href="#">Main</a>
+		            <h2>탑크리에듀 교육센터</h2>
+		            <a class="right" href="#">Menu</a>
+		        </header>
+		        
+		        <nav id="gnb">
+		            <div><a href="#">가입</a></div>
+		            <div><a href="#">소개</a></div>
+		            <div><a href="#">상품</a></div>
+		            <div><a href="#">board</a></div>
+		        </nav>
+		        
+		        <section>
+		            <h3>교육센터 소개</h3>
+		            <p>
+		                탑크리에듀는 정부지원 IT전문교육을 운영중입니다.
+		            </p>
+		        </section>
+		        
+		        <footer>
+		            &copy; Topcredu . Allrights reserved.
+		        </footer>
+		    </div>
+		</body>
+		</html>
+	 */
+
+}
