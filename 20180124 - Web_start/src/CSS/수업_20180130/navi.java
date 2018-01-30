3fb2ce12bb9fb7fcbcc7c71b2ab1c0b98e4f4cd3
Yeon <bsuyeon16@gamil.com>
2018-01-30 14:12:57 +0900

    CSS수업 - html, css파일 분리 실습 20180130

---
+++ 20180124 - Web_start/src/CSS/수업_20180130/navi.java
@@ -0,0 +1,75 @@
+package CSS.수업_20180130;
+
+public class navi {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>네비게이션 연습</title>
+		    <link rel="stylesheet" href="css/navi.css" />
+		</head>
+		<body>
+		    <!--  배경 동영상  -->
+		    <figure class="vid">
+		        <video loop="loop" autoplay="autoplay">
+		            <source src="movie/nike.mp4" type="video/mp4" />
+		        </video>
+		    </figure>
+		    
+		    <!--  로고  -->
+		    <h1><img src="img/logo2.png"></h1>
+		    
+		    <!--  header  -->
+		    <header>
+		        <ul id="gnb">
+		            <li>
+		                <a href="#"><em>About NIKE</em></a>
+		                <!--    서브메뉴    -->
+		                <ul>
+		                    <li><a href="#"><em>English</em></a></li>
+		                </ul>
+		            </li>
+		            <li>
+		                <a href="#"><em>Brand Info</em></a>
+		                <!--    서브메뉴    -->
+		                <ul>
+		                    <li><a href="#"><em>sub</em></a></li>
+		                    <li><a href="#"><em>sub</em></a></li>
+		                    <li><a href="#"><em>sub</em></a></li>
+		                    <li><a href="#"><em>sub</em></a></li>
+		                </ul>
+		            </li>
+		            <li>
+		                <a href="#"><em>Community</em></a>
+		                <!--    서브메뉴    -->
+		                <ul>
+		                    <li><a href="#"><em>sub</em></a></li>
+		                    <li><a href="#"><em>sub</em></a></li>
+		                    <li><a href="#"><em>sub</em></a></li>
+		                </ul>
+		            </li>
+		            <li>
+		                <a href="#"><em>Product Value</em></a>
+		                <!--    서브메뉴    -->
+		                <ul>
+		                    <li><a href="#"><em>sub</em></a></li>
+		                    <li><a href="#"><em>sub</em></a></li>
+		                </ul>
+		            </li>
+		            <li>
+		                <a href="#"><em>Company History</em></a>
+		                <!--    서브메뉴    -->
+		                <ul>
+		                    <li><a href="#"><em>sub</em></a></li>
+		                </ul>
+		            </li>
+		        </ul>
+		    </header>
+		    
+		</body>
+		</html>
+	 */
+
+}
---
+++ 20180124 - Web_start/src/CSS/수업_20180130/navi_css.java
@@ -0,0 +1,38 @@
+package CSS.수업_20180130;
+
+public class navi_css {
+	
+	/*
+	 * *{margin: 0; padding: 0;}
+		ol,ul {list-style: none;}
+		
+			배경동영상
+		.vid {width: 100%; height: 100%; position: fixed; top: -60px; left: 0px; opacity: 0.9;}
+		.vid video {width: 100%; min-width: 1600px;}
+		
+			로고
+		h1 {position: fixed; top: 40px; left: 40px; z-index: 10;}
+		h1 img {width: 280px; height: auto;}
+		header {width: 700px; height: 100%; position: fixed; top: 0px; left: -420px; 
+		    background: linear-gradient(-45deg,#e6fc55,#b9fb05,#97e031);}
+		
+			1 메뉴
+		#gnb {width: 220px; position: absolute; top: 150px; right: 0px;}
+		#gnb > li {position: relative;}
+		#gnb > li > a {display: block; width: 220px; height: 50px; font: bold 18px/3; color: #222; 
+		    box-sizing: border-box; padding-right: 40px; text-align: right;}
+		#gnb > li.on > a {background: #111; color: #fff;}
+		#gnb > li:hover > a {background: #111; color: #fff;}
+		#gnb em {display: inline-block;}
+		
+			2 서브메뉴
+		#gnb ul {position: absolute; left: 0px; top: 0px; opacity: 0; transition: all 0.4s;}
+		주메뉴에 마우스가 올라가면 서브메뉴가 보이게 하는 작업
+		#gnb > li:hover ul {opacity: 0.8; left: 205px;}
+		#gnb ul a {display: block; width: 150px; height: 40px; background: #222; 
+		    border-bottom: 1px solid #b9fb05; font: bold 16px/2.5; color: #fff; box-sizing: border-box; 
+		    padding-left: 50px; margin-bottom: 3px; transition: all 0.5s;}
+		#gnb ul a:hover {background: #b9fb05; color: #333;}
+	 */
+
+}
