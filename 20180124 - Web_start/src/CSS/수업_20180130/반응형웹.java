37974581365c2cd1f032f45c57e03a4388d75cae
Yeon <bsuyeon16@gamil.com>
2018-01-30 14:15:01 +0900

    반응형 웹 수업 - 기본 틀 실습 20180130

---
+++ 20180124 - Web_start/src/CSS/수업_20180130/반응형웹.java
@@ -0,0 +1,43 @@
+package CSS.수업_20180130;
+
+public class 반응형웹 {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Media Query Basic</title>
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		    <style>
+		        	반응형웹 - 사이즈에 따라 웹 , 아이패드,모바일 용으로 변형되는 웹 
+		        @media screen and(max-width: 767px){
+		            html{
+		                background: red;
+		                color:white; font-weight: bold;
+		            }
+		        }
+		
+		        @media screen and (min-width: 768px) and (max-width: 959px){
+		            html{
+		                background: green;
+		                color:white; font-weight: bold;
+		            }
+		        }        
+		
+		        @media screen and (min-width: 960px){
+		            html{
+		                background: blue;
+		                color:white; font-weight: bold;
+		            }
+		        }
+		    </style>   
+		</head>
+		<body>
+		    <h1>안녕하세요.</h1>
+		    <p> 반응형 웹입니다.</p>
+		</body>
+		</html>
+	 */
+
+}
