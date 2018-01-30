fad235d9820586fc150e5efa77db6514127c31c5
Yeon <bsuyeon16@gamil.com>
2018-01-30 15:07:33 +0900

    HTML&CSS 과제  20180130

---
+++ 과제/src/과제_20180130/HTML과제.java
@@ -0,0 +1,95 @@
+package 과제_20180130;
+
+public class HTML과제 {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>반응형 웹 과제</title>
+		    <style>
+		        #bg1 {background: red;}
+		        
+		        .bg2 {color: blue;}
+		        
+		        #red {color: red;}
+		        
+		        #red:nth-of-type(1) {color: red;}
+		        
+		        .blue {color: blue;}
+		        
+		        div > h1 {background: blue;}
+		        
+		        .bb {position: absolute; width: 100px; height: 50px;}
+		        .bb:hover {background: blue; color: white;}
+		    </style>
+		</head>
+		<body>
+		
+		
+		<!--1. email수신여부 예/아니오를 라디오버튼으로 작성하시오.-->
+		    <form>
+		       이메일 수신 여부 : 
+		        <input type="radio">예
+		        <input type="radio">아니오
+		    </form><br>
+		
+		
+		<!--2. html5,css,javascript를 선택할 수 있는 콤보박스(select)를 작성하시오.-->
+		    <form>
+		        <input type="checkbox" name="select">html5
+		        <input type="checkbox" name="select">css
+		        <input type="checkbox" name="select">javascript<br>
+		    </form><br>
+		
+		
+		<!--3. 배경색을 빨강색으로 변경하는 id 선택자 bg1을 작성하시오.-->
+		    <nav id="bg1">배경색이 빨강</nav><br>
+		
+		
+		<!--4. 글자색을 파랑색으로 변경하는 class 선택자 bg2를 작성하시오.-->
+		    <nav class="bg2">글자색이 파랑색</nav>
+		
+		
+		<!--5. id선택자와 class선택자의 차이점을 적으시오.-->
+		<!--id선택자의 경우 #id명칭라고 선택자를 표시하고 class는 .class명칭 이라고 표시해야 한다.-->
+		
+		
+		<!--
+		<참고소스코드>
+		    <p>HTML5</p>
+		    <p>HTML5</p>
+		    <div>
+		        <h1>HTML5</h1>
+		        <h2>HTML5 is a standard language for modern web.</h2>
+		    </div>
+		-->
+		
+		<!--6. 위코드에서 첫번째 p태그의 글자색을 빨강색으로 변경하시오. (id 사용)-->
+		    <p id="red">HTML5</p>
+		
+		
+		<!--7. 위코드에서 첫번째, 두번째 p태그의 글자색을 파랑색으로 변경하시오. (class사용)-->
+		    <p class="blue">HTML5</p>
+		    <div>
+		        <h1>HTML5</h1>
+		        <h2>HTML5 is a standard language for modern web.</h2>
+		    </div>
+		
+		
+		<!--8. 위코드에서 위코드에서 첫번째 p태그의 글자색을 빨강색으로 변경하시오. (nth-of-type사용)-->
+		
+		
+		<!--9. 위코드에서 h1태그의 배경색을 파랑색으로 변경하시오. (>사용)-->
+		
+		
+		<!--10. 버튼에 마우스가 올라가면 배경색을 파랑색, 글자색을 흰색으로 변경되게 하시오.-->
+		    <form>
+		        <input class="bb" type="submit">
+		    </form>
+		</body>
+		</html>
+	 */
+
+}
