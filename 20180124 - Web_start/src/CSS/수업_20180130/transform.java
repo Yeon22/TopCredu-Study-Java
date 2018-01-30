6604d8b54dbd4749cc35771be3193a9b77348771
Yeon <bsuyeon16@gamil.com>
2018-01-30 11:06:37 +0900

    CSS수업 - transform 20180130

---
+++ 20180124 - Web_start/src/CSS/수업_20180130/transform.java
@@ -0,0 +1,50 @@
+package CSS.수업_20180130;
+
+public class transform {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Document</title>
+		    <style>
+		        #box1{
+		            width: 100px;
+		            height: 100px;
+		            background-color: red;
+		            position: absolute;
+		            top: 300px;
+		            left: 100px;
+		            transition: 1s;
+		        }
+		        
+		        #box1:hover{
+		            transform: skewX(30deg); x축으로 기운다.
+		            transform: scale(2); 크기 2배로
+		            transform: rotateX(90deg); x에대한 축으로 돈다
+		            transform: rotateY(180deg); y에 대한 축으로 돈다
+		            transform: rotate(180deg); 풍차처럼 돈다
+		        }
+		        
+		        #box2{
+		            transform: translate(50px,50px);
+		            width: 200px;
+		            height: 200px;
+		            background-color: olive;
+		        }
+		    </style>
+		</head>
+		<body>
+		    <div id="box1">
+		        box1
+		    </div>
+		    
+		    <div id="box2">
+		        box2
+		    </div>
+		</body>
+		</html>
+	 */
+	
+}
