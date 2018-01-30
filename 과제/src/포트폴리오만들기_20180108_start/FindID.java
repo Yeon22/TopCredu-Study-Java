9ccf048dca63f3d0753da1890ad81c1a518d3b02
Yeon <bsuyeon16@gamil.com>
2018-01-30 16:18:26 +0900

    아이디, 비번찾기 클래스 만듬(아직 구현은 안함) 20180130

---
+++ 과제/src/포트폴리오만들기_20180108_start/FindID.java
@@ -0,0 +1,11 @@
+package 포트폴리오만들기_20180108_start;
+
+import javax.swing.JDialog;
+
+public class FindID extends JDialog{
+	
+	FindID(){
+		
+	}
+
+}
---
+++ 과제/src/포트폴리오만들기_20180108_start/FindPW.java
@@ -0,0 +1,11 @@
+package 포트폴리오만들기_20180108_start;
+
+import javax.swing.JDialog;
+
+public class FindPW extends JDialog{
+	
+	FindPW(){
+		
+	}
+
+}
