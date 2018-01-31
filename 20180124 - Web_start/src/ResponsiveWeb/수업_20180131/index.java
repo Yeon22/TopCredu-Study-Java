e19546a475c5f09d7266591f57c12deb262b897f
Yeon <bsuyeon16@gamil.com>
2018-01-31 15:26:49 +0900

    반응형 웹 만들기 - html파일 20180131

---
+++ 20180124 - Web_start/src/ResponsiveWeb/수업_20180131/index.java
@@ -0,0 +1,81 @@
+package ResponsiveWeb.¼ö¾÷_20180131;
+
+public class index {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		    <title>Responsive Web</title>
+		    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+		    <link rel="stylesheet" href="css/style.css" />
+		</head>
+		<body>
+		    <header>
+		        <h1>logo</h1>
+		        <ul id="gnb">
+		            <li><a href="#"><i class="fa fa-header"></i>Hotel</a></li>
+		            <li><a href="#"><i class="fa fa-building-o"></i>Fucility</a></li>
+		            <li><a href="#"><i class="fa fa-suitcase"></i>Business</a></li>
+		            <li><a href="#"><i class="fa fa-cutlery"></i>Restaurant</a></li>
+		        </ul>
+		        
+		        <div class="snsSet">
+		            <a href="#"><img src="img/sns1.png" alt="about" /></a>
+		            <a href="#"><img src="img/sns2.png" alt="about" /></a>
+		            <a href="#"><img src="img/sns3.png" alt="about" /></a>
+		            <a href="#"><img src="img/sns4.png" alt="about" /></a>
+		            <a href="#"><img src="img/sns5.png" alt="about" /></a>
+		        </div>
+		        
+		    </header>
+		    
+		    <nav>
+		        
+		    </nav>
+		    
+		    <section>
+		        <article>
+		            
+		        </article>
+		        <article>
+		            
+		        </article>
+		        <article>
+		            
+		        </article>
+		        <article>
+		            
+		        </article>
+		        <article>
+		            
+		        </article>
+		        <article>
+		            
+		        </article>
+		        <article>
+		            
+		        </article>
+		        <article>
+		            
+		        </article>
+		        <article>
+		            
+		        </article>
+		        <article>
+		            
+		        </article>
+		        <article>
+		            
+		        </article>
+		        <article>
+		            
+		        </article>
+		    </section>
+		</body>
+		</html>
+	 */
+
+}
