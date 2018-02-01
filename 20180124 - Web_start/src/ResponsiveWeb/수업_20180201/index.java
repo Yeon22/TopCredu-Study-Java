6d97aedbca5497629d2c43fd7fb7c4f16d7163d7
Yeon <bsuyeon16@gamil.com>
2018-02-01 12:23:53 +0900

    반응형 웹 만들기 - html파일 완성 20180201

---
+++ 20180124 - Web_start/src/ResponsiveWeb/수업_20180201/index.java
@@ -0,0 +1,130 @@
+package ResponsiveWeb.¼ö¾÷_20180201;
+
+public class index {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		    <title>Responsive Web</title>
+		    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+		    <link rel="stylesheet" href="css/style.css" />
+		</head>
+		<body>
+		    <header>
+		        <h1>logo</h1>
+		        <ul id="gnb">
+		            <li><a href="#"><i class="fa fa-header"></i>Hotel</a></li>
+		            <li><a href="#"><i class="fa fa-building-o"></i>Fucility</a></li>
+		            <li><a href="#"><i class="fa fa-suitcase"></i>Business</a></li>
+		            <li><a href="#"><i class="fa fa-cutlery"></i>Restaurant</a></li>
+		        </ul>
+		        
+		        <div class="snsSet">
+		            <a href="#"><img src="img/sns1.png" alt="about" /></a>
+		            <a href="#"><img src="img/sns2.png" alt="about" /></a>
+		            <a href="#"><img src="img/sns3.png" alt="about" /></a>
+		            <a href="#"><img src="img/sns4.png" alt="about" /></a>
+		            <a href="#"><img src="img/sns5.png" alt="about" /></a>
+		        </div>
+		        
+		    </header>
+		    
+		    <nav>
+		        <a href="#"><img src="img/util1.png" alt="" /></a>
+		        <a href="#"><img src="img/util2.png" alt="" /></a>
+		        <a href="#"><img src="img/util3.png" alt="" /></a>
+		        <a href="#"><img src="img/util4.png" alt="" /></a>
+		    </nav>
+		    
+		    <section>
+		        <article>
+		            <div class="bg bg1">
+		                <span>Maldives</span>
+		            </div>
+		        </article>
+		        <article>
+		            <div class="txt">
+		                <i class="fa fa-arrow-right"></i>
+		                <p>
+		                    <strong>A Whole New World</strong>
+		                    <em>I Can show you the world. Shining Shimmering Splended.
+		                        <br> Tell me when you decide let me be in your heart
+		                    </em>
+		                </p>
+		            </div>
+		        </article>
+		        <article>
+		            <div class="txt">
+		                <i class="fa fa-spin fa-refresh"></i>
+		                <p><strong>Reservation</strong></p>
+		            </div>
+		        </article>
+		        <article>
+		            <div class="bg bg2">
+		                <span>Suite</span>
+		            </div>
+		        </article>
+		        <article>
+		            <div class="bg bg3">
+		                <span>Surf</span>
+		            </div>
+		        </article>
+		        <article>
+		            <div class="txt">
+		                <i class="fa fa-arrow-right"></i>
+		                <p>
+		                    <strong>A Dazzling Place</strong><br>
+		                    <em>
+		                        I can show you the world. Shining Shimmering Splended.
+		                        <br> Tell me when you decide let me be in your heart
+		                    </em>
+		                </p>
+		            </div>
+		        </article>
+		        <article>
+		            <div class="bg bg4">
+		                <span>Sauna</span>
+		            </div>
+		        </article>
+		        <article>
+		            <div class="txt">
+		                <i class="fa fa-arrow-right"></i>
+		                <strong>Hot spot</strong>
+		                <em> a best place to have a vaction <br>
+		                    Tell me when you
+		                </em>
+		            </div>
+		        </article>
+		        <article>
+		            <div class="bg bg5">
+		                <span>Dinner</span>
+		            </div>
+		        </article>
+		        <article>
+		            <div class="bg bg6">
+		                <span>Spa</span>
+		            </div>
+		        </article>
+		        <article>
+		            <div class="txt">
+		                <i class="fa fa-arrow-right"></i>
+		                <p>
+		                    <strong>Open Space</strong>
+		                    <em>I Can show you the world</em>
+		                </p>
+		            </div>
+		        </article>
+		        <article>
+		            <div class="bg bg7">
+		                <span>Tour</span>
+		            </div>
+		        </article>
+		    </section>
+		</body>
+		</html>
+	 */
+
+}
