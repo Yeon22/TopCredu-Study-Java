d85a592c6adfcffe19f52663733f4f6735b930eb
Yeon <bsuyeon16@gamil.com>
2018-02-02 16:09:22 +0900

    HTML + CSS 과제 20180202

---
+++ 과제/src/과제_20180202/HTML_CSS과제.java
@@ -0,0 +1,118 @@
+package 과제_20180202;
+
+public class HTML_CSS과제 {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>html과제</title>
+		    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+		    <style>
+				14. css를 작성할 때 모든 엘리먼트의 여백을 없애기 위한 코드를 작성하시오.   
+		        * { padding: 0; margin: 0; }
+		        
+		        article > div { border: 1px solid black; float: left; width: 33%; }
+		        
+		        ul { list-style: none; }
+		        ul > li { float: left; width: 25%; }
+		        
+		        input { color: white; background-color: red; }
+		        p > input:hover { background-color: green; }
+		        
+		        @import url(//fornts.googleapis.com/earlyaccess/nanumpenscript.css);
+		        h1 { font-family: 'Nanum Pen Script', cursive; }
+		        
+		        img { width: 100px; height: 100px; margin-left: 100px; border-radius: 10px 10px 10px 10px; box-shadow: 10px 5px 5px black;}
+		        
+		        video {width: 100%; height: 100%; top: -60px; left: 0px; opacity: 0.9;}
+		    </style>
+		</head>
+		<body>
+		   
+		    1. bracket 에디터에서 emmet 플러그인의 역할을 간단히 적으시오.
+		    
+		    <p>간단한 단축키로 명령어를 칠 수 있다.</p>
+		
+		
+		    2. utf-8로 인코딩하기 위한 meta 태그를 적으시오.
+		
+		    <p> meta charset="utf-8" </p>
+		
+		   
+		    3. 머릿말,네비게이션,본문,꼬릿말등을 표현하는 시맨틱태그를 적으시오.
+		
+		    <p>header, nav, article, footer</p>
+		
+		
+		    4. div와 span의 차이를 적으시오.<br />
+		
+		    <p>div - 블럭으로 지정된다. <br />
+		        span - 태그 요소에 지정된다.
+		    </p>
+		
+		
+		    5. 세개의 박스(div)를 옆으로 나란히 배치하도록 작성하시오.<br />
+		    6. 5번의 박스들 밑에 꼬릿말이 오도록 작성하시오.
+		
+		    <article>
+		    <div>first box</div>
+		    <div>second box</div>
+		    <div>third box</div>
+		    </article>
+		    <footer>꼬릿말</footer><p></p>
+		
+		   
+		    7. 목록을 만들어서 다음과 같이 옆으로 보이도록 작성하시오.<br />
+		        Home 회사소개 회사연혁 회사비전<br />
+		
+		    <ul>
+		        <li>Home</li>
+		        <li>회사소개</li>
+		        <li>회사연혁</li>
+		        <li>회사비전</li>
+		    </ul>
+		    
+		
+		    8. 빨간색 등록 버튼을 만들어서 마우스오버시 배경색이 녹색이 되게 하시오.
+		    <p>
+		        <input type="button" value="등록" />
+		    </p>
+		    
+		
+		    9. 구글폰트중 나눔고딕 폰트를 다음코드에 적용하시오.<br />
+		    <h1>웹표준</h1>
+		
+		
+		    10. font awesome아이콘 중 달력모양 아이콘을 출력하시오.<br />
+		    
+		    <i class="fas fa-calendar-times"></i><br />
+		    
+		    
+		    11. viewport메타태그를 작성하고, 기능을 간단히 설명하시오.<br />
+		    
+		    <p>
+		        meta name="viewport" content="width=device-width, initial-scale=1.0"<br />
+		        창 크기조절을 할 때 사용하며 주로 반응형 웹을 만들 때 사용한다.
+		    </p><br />
+		   
+		   
+		    12. 이미지의 모서리를 둥글게 하고, 그림자가 나타나도록 하시오.<br />
+		
+		    <img src="img/coffee.jpg" alt="about">
+		    <br />
+		
+		
+		    13. 유튜브에서 시빌워예고편을 찾아서, 예고편 동영상이 실행되도록 작성하시오.<br />
+		    
+		    <video loop="loop" autoplay="autoplay">
+		        <source src="video/civilwar.mp4" type="video/mp4" />
+		    </video>
+		    
+		
+		</body>
+		</html>
+	 */
+
+}
