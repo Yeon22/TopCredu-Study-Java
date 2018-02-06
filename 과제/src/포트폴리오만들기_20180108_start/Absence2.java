8b38bd6a7fdf654df15366ac7ff81de343c65755
Yeon <bsuyeon16@gamil.com>
2018-02-06 15:12:58 +0900

    출석현황 구현 완료 20180206

--- 과제/src/포트폴리오만들기_20180108_start/Absence2.java
+++ 과제/src/포트폴리오만들기_20180108_start/Absence2.java
@@ -1,13 +1,17 @@
 package 포트폴리오만들기_20180108_start;
 
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -17,7 +21,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-public class Absence2 extends JPanel{
+public class Absence2 extends JPanel implements Runnable{
 	Connection conn = null;
 	Statement stmt = null;
 	String query;
@@ -26,6 +30,54 @@ public class Absence2 extends JPanel{
 	JTable table;
 	
 	JButton searchBtn = new JButton("조회");
+	JLabel timeLabel;
+
+	@Override
+	public void run() {
+		while(true) {
+			Calendar cal = Calendar.getInstance();
+			String now = cal.get(Calendar.YEAR)+"년"
+					+(cal.get(Calendar.MONTH)+1)+"월"
+					+cal.get(Calendar.DATE)+"일";
+			timeLabel.setText(now);
+			
+			try {
+				Thread.sleep(1000);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	public void List(String squery) {
+		try {
+			if(squery.equals("")) {
+				query = "select class_id, name, checkme from pofol_score order by class_id";
+			} else {
+				query = "select class_id, name, checkme from pofol_score "+squery+" order by class_id";
+			}
+			
+			ResultSet rs = stmt.executeQuery(query);
+			
+			model.setNumRows(0);
+			
+			while(rs.next()) {
+				String[] row = new String[3];
+				row[0] = rs.getString("class_id");
+				row[1] = rs.getString("name");
+				row[2] = rs.getString("checkme");
+				
+				model.addRow(row);
+			}
+			
+			rs.close();
+			
+		}catch(SQLException e) {
+			e.getStackTrace();
+		}
+	}
+	
 	
 	Absence2(){
 		String url = null;
@@ -46,7 +98,7 @@ public class Absence2 extends JPanel{
 		setLayout(null);
 		
 		JLabel department = new JLabel("학과 : ");
-		department.setBounds(20,10,45,25);
+		department.setBounds(28,10,45,25);
 		add(department);
 		
 		String dept[] = {"전체", "컴퓨터공학과", "멀티미디어과","세무회계과","독어독문과"};
@@ -81,9 +133,17 @@ public class Absence2 extends JPanel{
 		});
 		
 		
-		JLabel w_date = new JLabel("날짜 : ");
-		w_date.setBounds(48,370,70,30);
+		JLabel w_date = new JLabel("오늘 날짜 : ");
+		w_date.setBounds(240,8,70,30);
 		add(w_date);
+		
+		timeLabel = new JLabel();
+		timeLabel.setFont(new Font("Gothic", Font.BOLD, 16));
+		timeLabel.setBounds(305,8,120,30);
+		add(timeLabel);
+		
+		Thread thread = new Thread(this);
+		thread.start();
 		
 		
 		
@@ -92,7 +152,28 @@ public class Absence2 extends JPanel{
 		searchBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				int index = cb_dept.getSelectedIndex();
+				String cb_name = cb_dept.getItemAt(index).toString();
 				
+				if(cb_name.equals("컴퓨터공학과")) {
+					String squery = "where department_id = "
+							+"(select department_id from pofol_department where department_id = 'C001')";
+					List(squery);
+				} else if(cb_name.equals("멀티미디어과")) {
+					String squery = "where department_id = "
+							+"(select department_id from pofol_department where department_id = 'M002')";
+					List(squery);
+				} else if(cb_name.equals("세무회계과")) {
+					String squery = "where department_id = "
+							+"(select department_id from pofol_department where department_id = 'T003')";
+					List(squery);
+				} else if(cb_name.equals("독어독문과")) {
+					String squery = "where department_id = "
+							+"(select department_id from pofol_department where department_id = 'G004')";
+					List(squery);
+				} else {
+					List("");
+				}
 			}
 		});
 		
