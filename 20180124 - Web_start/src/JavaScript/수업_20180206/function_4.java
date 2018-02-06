5609df264227fce78db910d22672fa7e0a75f4ca
Yeon <bsuyeon16@gamil.com>
2018-02-06 14:30:10 +0900

    JavaScript 수업 20180206

---
+++ 20180124 - Web_start/src/JavaScript/수업_20180206/function_4.java
@@ -0,0 +1,36 @@
+package JavaScript.수업_20180206;
+
+public class function_4 {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Document</title>
+		    <script>
+		        function login(id, pw){
+		            if(id == "hanguk"){
+		                if(pw == "1234"){
+		                    document.write(id + "님 방문을 환영합니다.");
+		                }else{
+		                    alert("잘못된 비밀번호입니다.");
+		                }
+		            }else{
+		                alert("존재하지 않는 아이디입니다.");
+		            }
+		        }
+		        
+		        var user_id = prompt("아이디를 입력하세요.","");
+		        var user_pw = prompt("패스워드를 입력하세요.","")
+		        
+		        login(user_id, user_pw);
+		    </script>
+		</head>
+		<body>
+		    
+		</body>
+		</html>
+	 */
+
+}
