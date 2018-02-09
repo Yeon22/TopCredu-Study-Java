e193747f1e0e50bea912919f7692d64b633b6f03
Yeon <bsuyeon16@gamil.com>
2018-02-09 12:23:53 +0900

    반응형 이미지 만들기 수업 20180209

---
+++ 20180124 - Web_start/src/JQuery/수업_20180209/ResponsiveGallery.java
@@ -0,0 +1,123 @@
+package JQuery.수업_20180209;
+
+public class ResponsiveGallery {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Document</title>
+		    <style>
+		    @import url(https://cdn.rawgit.com/openhiun/hangul/14c0f6faa2941116bb53001d6a7dcd5e82300c3f/nanumbarungothic.css);
+		        
+		        * { box-sizing: border-box; margin: 0; padding: 0; }
+		        body { font-family: "Nanum Barun Gothic","Ubuntu Condensed","Noto Sans Korean"; }
+		        
+		        .gallery_content:hover .overlay{ top: 0; }
+		        .gallery_layout { width: 960px; margin: 0 auto; }
+		        #gallery_layout:after { content: ""; display: block; clear: both; }
+		        .gallery_content { position: relative; float: left; width: 33.3333333; overflow: hidden; }
+		        .gallery_content img { width: 100%; height: 300px; display: block; margin: 0 auto; transition: all 1s ease-in-out; }
+		        .content { z-index: 1; display: none; color: #fff; position: absolute; top: 50%; left: 50%; transform: translate(-50%,-50%); }
+		        .content h1 { font-size: 1.2em; text-transform: uppercase; text-align: center; }
+		        .content p { padding: 2px 0; font-size: 0.8em; text-align: center; }
+		        .gallery_content:hover .content{ display: block; transition: all 1s ease-in-out; }
+		        .gallery_content:hover img { transform: scale(1.2); transition: all 1s ease-in-out; }
+		        .overlay { display: none; width: 100%; height: 100%; position: absolute; top: 0; left: 0; right: 0; bottom: 0; transition: top 0.3s, right 0.3s, bottom 0.3s, left 0.3s; }
+		        
+				// overlay color 
+		        .overlay.darkBlue { background-color: rgba(31, 50, 99, 0.6); }
+		        .overlay.gold { background-color: rgba(216, 177, 126, 0.5); }
+		        .overlay.pink { background-color: rgba(255, 113, 181, 0.7); }
+		        .overlay.black { background-color: rgba(0, 0, 0, 0.5); }
+		        .overlay.yellow { background-color: rgba(103, 58, 183, 0.5); }
+		        .overlay.red { background-color: rgba(244, 67, 54, 0.5); }
+		        
+		        .gallery_content:hover .overlay{ display: block; transition: all 0.5s ease-in-out; }
+		        
+		        
+		        @media all and (min-width: 960px) {
+		            #gallery_layout {
+		                position: absolute;
+		                top: 50%;
+		                left: 50%;
+		                transform: translate(-50%, -50%);
+		            }  
+		        }
+		
+		        @media all and (max-width: 959px) {
+		            #gallery_layout {
+		                width: 100%;
+		            }
+		            .gallery_content {
+		                width: 50%;
+		            }
+		        }
+		        
+		        @media all and (max-width: 499px) {
+		            #gallery_layout {
+		                width: 100%;
+		            }
+		            .gallery_content {
+		                width: 100%;
+		            }
+		        }
+		    </style>
+		</head>
+		<body>
+		    <div id="gallery_layout">
+		        <div class="gallery_content">
+		            <img src="images/animal_1.jpg" alt="animal_img">
+		            <div class="content">
+		                <h1>Animal</h1>
+		                <p>그 아이를 끝까지 책임질 자신이 있는가.</p>
+		            </div>
+		            <div class="overlay darkBlue"></div>
+		        </div>
+		        <div class="gallery_content">
+		            <img src="images/animal_2.jpg" alt="animal_img">
+		            <div class="content">
+		                <h1>Animal</h1>
+		                <p>끝까지 책임질게 아니면 시작도 하지 마라.</p>
+		            </div>
+		            <div class="overlay red"></div>
+		        </div>
+		        <div class="gallery_content">
+		            <img src="images/animal_3.jpg" alt="animal_img">
+		            <div class="content">
+		                <h1>Animal</h1>
+		                <p>자신의 목숨을 다하여 사랑해 줘라.</p>
+		            </div>
+		            <div class="overlay pink"></div>
+		        </div>
+		        <div class="gallery_content">
+		            <img src="images/animal_4.jpg" alt="animal_img">
+		            <div class="content">
+		                <h1>Animal</h1>
+		                <p>돈 없으면 키우지 마라.</p>
+		            </div>
+		            <div class="overlay black"></div>
+		        </div>
+		        <div class="gallery_content">
+		            <img src="images/animal_5.jpg" alt="animal_img">
+		            <div class="content">
+		                <h1>Animal</h1>
+		                <p>후회를 남기지 마라. 그리움만 남겨라.</p>
+		            </div>
+		            <div class="overlay yellow"></div>
+		        </div>
+		        <div class="gallery_content">
+		            <img src="images/animal_6.jpg" alt="animal_img">
+		            <div class="content">
+		                <h1>Animal</h1>
+		                <p>당신은 그 아이에게 최고의 사랑을 받을 것이다. 당신이 버리지 않는 한</p>
+		            </div>
+		            <div class="overlay gold"></div>
+		        </div>
+		    </div>
+		</body>
+		</html>
+	 */
+
+}
