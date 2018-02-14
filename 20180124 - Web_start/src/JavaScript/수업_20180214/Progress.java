2c5afeec0ff0272952432c1494feaaeb5bb21166
Yeon <bsuyeon16@gamil.com>
2018-02-14 12:13:38 +0900

    프로그레스바 만들기 20180214

---
+++ 20180124 - Web_start/src/JavaScript/수업_20180214/Progress.java
@@ -0,0 +1,45 @@
+package JavaScript.수업_20180214;
+
+public class Progress {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Document</title>
+		    <style>
+		        #myProgress {width: 100%; background-color: #ddd;}
+		        
+		        #myBar {width: 1%; height: 30px; background-color: #4CAF50;}
+		    </style>
+		</head>
+		<body>
+		    <h1>자바스크립트 프로그레스바</h1>
+		    <div id="myProgress">
+		        <div id="myBar"></div>
+		    </div>
+		    <br>
+		    <button onclick="move()">시작</button>
+		    
+		    <script>
+		        function move(){
+		            var elem = document.getElementById("myBar");
+		            var width = 1;
+		            var id = setInterval(frame,10);
+		            
+		            function frame(){
+		                if(width >= 100){
+		                    clearInterval(id);
+		                }else{
+		                    width++;
+		                    elem.style.width = width + "%";
+		                }
+		            }
+		        }
+		    </script>
+		</body>
+		</html>
+	 */
+
+}
