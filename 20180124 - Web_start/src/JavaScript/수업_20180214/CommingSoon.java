30272b878899846a1df7730544692f77d521e59a
Yeon <bsuyeon16@gamil.com>
2018-02-14 14:30:14 +0900

    Comming Soon Page 만들기 20180214

---
+++ 20180124 - Web_start/src/JavaScript/수업_20180214/CommingSoon.java
@@ -0,0 +1,98 @@
+package JavaScript.수업_20180214;
+
+public class CommingSoon {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Document</title>
+		    <style>
+		        body, html {
+		            height: 100%;
+		            margin: 0;
+		        }
+		        .bgimg {
+		            background-image: url(img2/img_maplestory.jpg);
+		            height: 100%;
+		            background-position: center;
+		            background-size: cover;
+		            position: relative;
+		            color: white;
+		            font-family: "Courier New", Courier, monospace;
+		            font-size: 25px;
+		        }
+		        .topleft {
+		            position: absolute;
+		            top: 0;
+		            left: 16px;
+		        }
+		        .middle {
+		            position: absolute;
+		            top: 50%;
+		            left: 50%;
+		            transform: translate(-50%, -50%);
+		            text-align: center;
+		        }
+		        button {
+		            width: 150px;
+		            height: 50px;
+		            font-size: 20px;
+		            color: white;
+		            background-color: black;
+		            border: 1px solid white;
+		        }
+		        button:hover {
+		            background-color: dimgray;
+		        }
+		    </style>
+		</head>
+		<body>
+		    <div class="bgimg">
+		        <div class="topleft">
+		            <p>Nexon Soft</p>
+		        </div>
+		        <div class="middle">
+		            <h1>COMMING SOON</h1>
+		            <hr>
+		            <p id="demo"></p>
+		            <button>사전예약</button>
+		        </div>
+		    </div>
+		    
+		    <script>
+		        // Set the date we're counting down to
+		        var countDownDate = new Date("Feb 20, 2018 15:37:25").getTime();
+		
+		        // Update the count down every 1 second
+		        var x = setInterval(function() {
+		
+		        // Get todays date and time
+		        var now = new Date().getTime();
+		
+		        // Find the distance between now an the count down date
+		        var distance = countDownDate - now;
+		
+		        // Time calculations for days, hours, minutes and seconds
+		        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		
+		        // Display the result in the element with id="demo"
+		        document.getElementById("demo").innerHTML = days + "일 " + hours + "시 "
+		        + minutes + "분 " + seconds + "초 ";
+		
+		        // If the count down is finished, write some text 
+		        if (distance < 0) {
+		            clearInterval(x);
+		            document.getElementById("demo").innerHTML = "EXPIRED";
+		          }
+		        }, 1000);
+		    </script>
+		</body>
+		</html>
+	 */
+
+}
