f8e0ba758da1ab188b2afe93e729d62c9f2d8b5e
Yeon <bsuyeon16@gamil.com>
2018-02-14 14:06:09 +0900

    타이핑 느낌 글쓰기 20180214

---
+++ 20180124 - Web_start/src/JavaScript/수업_20180214/Tipping.java
@@ -0,0 +1,33 @@
+package JavaScript.수업_20180214;
+
+public class Tipping {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Document</title>
+		</head>
+		<body>
+		    <h1>타이핑느낌 글쓰기</h1>
+		    <button onclick="typeWriter()">클릭</button>
+		    <p id="demo"></p>
+		    <script>
+		        var i = 0;
+		        var txt = '육지담, 강다니엘 이전 감정 정리했다';
+		        var speed = 50;
+		        
+		        function typeWriter(){
+		            if(i < txt.length){
+		                document.getElementById("demo").innerHTML += txt.charAt(i);
+		                i++;
+		                setTimeout(typeWriter, speed);
+		            }
+		        }
+		    </script>
+		</body>
+		</html>
+	 */
+
+}
