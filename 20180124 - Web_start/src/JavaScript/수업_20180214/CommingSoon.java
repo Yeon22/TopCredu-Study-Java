df97dea53e0b8cc854e031364023f619ecca867a
Yeon <bsuyeon16@gamil.com>
2018-02-14 16:52:04 +0900

    커밍순페이지 만들기 - 띄어쓰기 수정 20180214

--- 20180124 - Web_start/src/JavaScript/수업_20180214/CommingSoon.java
+++ 20180124 - Web_start/src/JavaScript/수업_20180214/CommingSoon.java
@@ -68,27 +68,28 @@ public class CommingSoon {
 		        // Update the count down every 1 second
 		        var x = setInterval(function() {
 		
-		        // Get todays date and time
-		        var now = new Date().getTime();
-		
-		        // Find the distance between now an the count down date
-		        var distance = countDownDate - now;
-		
-		        // Time calculations for days, hours, minutes and seconds
-		        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-		
-		        // Display the result in the element with id="demo"
-		        document.getElementById("demo").innerHTML = days + "일 " + hours + "시 "
-		        + minutes + "분 " + seconds + "초 ";
-		
-		        // If the count down is finished, write some text 
-		        if (distance < 0) {
-		            clearInterval(x);
-		            document.getElementById("demo").innerHTML = "EXPIRED";
-		          }
+			        // Get todays date and time
+			        var now = new Date().getTime();
+			
+			        // Find the distance between now an the count down date
+			        var distance = countDownDate - now;
+			
+			        // Time calculations for days, hours, minutes and seconds
+			        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+			
+			        // Display the result in the element with id="demo"
+			        document.getElementById("demo").innerHTML = days + "일 " + hours + "시 "
+			        + minutes + "분 " + seconds + "초 ";
+			
+			        // If the count down is finished, write some text 
+			        if (distance < 0) {
+			            clearInterval(x);
+			            document.getElementById("demo").innerHTML = "EXPIRED";
+			          }
+			          
 		        }, 1000);
 		    </script>
 		</body>
