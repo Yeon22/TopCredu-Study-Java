3a6764dfb109ad3dc46d8df6ad6d8be958e6cb03
Yeon <bsuyeon16@gamil.com>
2018-02-14 12:13:25 +0900

    별점 만들기 20180214

---
+++ 20180124 - Web_start/src/JavaScript/수업_20180214/별점.java
@@ -0,0 +1,27 @@
+package JavaScript.수업_20180214;
+
+public class 별점 {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		    <title>별점</title>
+		    <style>
+		        .checked {color: red;}
+		    </style>
+		</head>
+		<body>
+		    <h2>별점</h2>
+		    <span class="fa fa-star checked"></span>
+		    <span class="fa fa-star checked"></span>
+		    <span class="fa fa-star checked"></span>
+		    <span class="fa fa-star"></span>
+		    <span class="fa fa-star"></span>
+		</body>
+		</html>
+	 */
+
+}
