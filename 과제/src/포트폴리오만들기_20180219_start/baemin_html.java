545b69cc651a25942e4245c5f238ad2948143cc2
Yeon <bsuyeon16@gamil.com>
2018-02-19 18:11:11 +0900

    웹 포트폴리오 만들기 - 레이아웃 20180219

---
+++ 과제/src/포트폴리오만들기_20180219_start/baemin_html.java
@@ -0,0 +1,82 @@
+package 포트폴리오만들기_20180219_start;
+
+public class baemin_html {
+	
+	/* 배달의 민족 홈페이지 만들기
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+		    <link rel="shortcut icon" href="favicon.ico">
+		    <link rel="icon" type="image/gif" href="images_20180219/animated_favicon1.gif">
+		    <link rel="stylesheet" href="style.css">
+		    <title>대한민국 1등 배달앱, 배달의 민족</title>
+		</head>
+		<body>
+		    <div class="container">
+		       <header>
+		           <div class="top">
+		                <div id="logo">
+		                    <!--배달의 민족 로고-->
+		                    <img src="images_20180219/baemin.jpg" alt="배달의민족" style="width: 280px; height: 150px;" />
+		                </div>
+		                <div id="snsIcon">
+		                    <!--SNS icon(fontawesome) - facebook, instagram, youtube-->
+		                    <a href="https://www.facebook.com/smartbaedal"><i class="fab fa-facebook-f"></i></a>
+		                    <a href="https://www.instagram.com/baemin_official/"><i class="fab fa-instagram"></i></a>
+		                    <a href="https://www.youtube.com/user/smartbaedal2"><i class="fab fa-youtube-square"></i></a>
+		                </div>
+		            </div>
+		       </header>
+		        <div class="section-wrap">
+		            <div class="section-header">
+		                <!--맨 처음 음식사진 나오는 부분 1번째 content-->
+		                <div class="header-content">
+		                    <p>
+					                        가볍게<br>
+					                        맛있게<br>
+					                        먹고 싶을 때
+		                    </p>
+		                </div>
+		            </div>
+		            <div class="section-content">
+		                <!--2번째 content-->
+		                <div class="content-text">
+		                    <p>
+					                        바로 그거! <br>
+					                        배민에 <br>
+					                        다~있다 
+		                    </p>
+		                </div>
+		            </div>
+		            <div class="section-content">
+		                <!--3번째 content-->
+		                <div class="content-text">
+		                    <p>
+					                        줄 서서 <br>
+					                        먹던 맛집도 <br>
+					                        우리집으로
+		                    </p>
+		                </div>
+		            </div>
+		            <div class="section-content">
+		                <!--4번째 content-->
+		                <div class="content-text">
+		                    <p>
+					                        어떤 <br>
+					                        결재방법이든 <br>
+		                       10초면 끝!
+		                    </p>
+		                </div>
+		            </div>
+		            <footer class="section-footer">
+		                <!--마지막 content-->
+		            </footer>
+		        </div>
+		    </div>
+		</body>
+		</html>
+	 */
+
+}
---
+++ 과제/src/포트폴리오만들기_20180219_start/style_css.java
@@ -0,0 +1,9 @@
+package 포트폴리오만들기_20180219_start;
+
+public class style_css {
+	
+	/*
+	 * 
+	 */
+
+}
