fec6fad7791c41e1645330647a00d5928524422a
Yeon <bsuyeon16@gamil.com>
2018-02-19 14:21:59 +0900

    JQuery수업 - bxSlider 20180219

---
+++ 20180124 - Web_start/src/JQuery/수업_20180219/bxSlider_html.java
@@ -0,0 +1,66 @@
+package JQuery.수업_20180219;
+
+public class bxSlider_html {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Document</title>
+		    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+		    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		    <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+		    <script>
+		        $(function(){
+		            var mySlider = $("#slide_banner").bxSlider({
+		                mode:"horizontal", //가로 수평으로 슬라이드 된다.
+		                speed:500, //이동 속도를 설정한다.
+		                pager:false, //현재 위치 페이징 표시 여부 설정
+		                moveSlides:1, //슬라이드 이동시 갯수 설정
+		                slideWidth:90, //슬라이드 마다 너비 설정
+		                minSlides:4, //최소 노출 개수를 설정
+		                maxSlides:4, //최대 노출 개수를 설정
+		                slideMargin:10, //슬라이드간의 간격을 설정
+		                auto:true, //자동으로 흐를지 설정
+		                autoHover:true, //마우스 오버시 정지시킬지 설정
+		                controls:false //이전 다음 버튼 노출 여부 설정
+		            });
+		            
+		            //이전 버튼을 클릭하면 이전 슬라이드로 전환된다.
+		            $("#prevBtn").on("click", function(){
+		                mySlider.goToPrevSlide(); //이전 슬라이드 배너로 이동된다.
+		                return false; //<a>에 링크를 차단한다.
+		            });
+		            
+		            //다음 버튼을 클릭하면 다음 슬라이드로 전환된다.
+		            $("#nextBtn").on("click", function(){
+		                mySlider.goToNextSlide(); //다음 슬라이드 배너로 이동된다.
+		                return false;
+		            });
+		        });
+		    </script>
+		    <style type="text/css">
+		        *{margin: 0; padding: 0;}
+		        img{border: none; vertical-align: top;}
+		        #banner_wrap{position: relative; width: 470px; margin: 0 auto;}
+		        #prevBtn{position: absolute; left: 0; top: 10px;}
+		        #nextBtn{position: absolute; right: 15px; top: 10px;}
+		    </style>
+		</head>
+		<body>
+		    <div id="banner_wrap">
+		        <ul id="slide_banner">
+		            <li><a href="#"><img src="img/animal_1.jpg" alt="사진1" /></a></li>
+		            <li><a href="#"><img src="img/animal_2.jpg" alt="사진2" /></a></li>
+		            <li><a href="#"><img src="img/animal_3.jpg" alt="사진3" /></a></li>
+		            <li><a href="#"><img src="img/animal_4.jpg" alt="사진4" /></a></li>
+		        </ul>
+		        <p><a href="#" id="prevBtn"><img src="img/prev_btn.png" alt="이전버튼"></a></p>
+		        <p><a href="#" id="nextBtn"><img src="img/next_btn.png" alt="다음버튼"></a></p>
+		    </div>
+		</body>
+		</html>
+	 */
+
+}
