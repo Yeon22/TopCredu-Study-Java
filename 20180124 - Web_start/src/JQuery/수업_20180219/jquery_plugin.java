1c19d3b5c305482657a07533b56fa86d8dfb1b44
Yeon <bsuyeon16@gamil.com>
2018-02-19 14:21:39 +0900

    JQuery수업 - 플러그인 20180219

---
+++ 20180124 - Web_start/src/JQuery/수업_20180219/jquery_plugin.java
@@ -0,0 +1,32 @@
+package JQuery.¼ö¾÷_20180219;
+
+public class jquery_plugin {
+	
+	/*
+	 * <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		    <meta charset="UTF-8">
+		    <title>Document</title>
+		    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+		    <script type="text/javascript" src="js/jquery-ui.js"></script>
+		    <script>
+		        $(function(){
+		            $("#box").draggable();
+		        });
+		    </script>
+		    <style>
+		        #box{
+		            width: 100px;
+		            height: 100px;
+		            background-color: salmon;
+		        }
+		    </style>
+		</head>
+		<body>
+		    <div id="box"></div>
+		</body>
+		</html>
+	 */
+
+}
