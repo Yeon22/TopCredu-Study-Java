ab9aaf7a4d6a6f3cadc3dc956d6fe67d6212bc46
Yeon <bsuyeon16@gamil.com>
2018-02-22 10:25:44 +0900

    JSP,Servlet 수업 - 체크박스 20180222

---
+++ 20180221 - web-study-02/WebContent/checkbox.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h2>악세서리</h2>
+	관심항목을 선택하세요.
+	<hr>
+	<form method="get" action="CheckboxServlet">
+		<input type="checkbox" name="item" value="신발" />신발
+		<input type="checkbox" name="item" value="가방" />가방
+		<input type="checkbox" name="item" value="벨트" />벨트<br>
+		<input type="checkbox" name="item" value="모자" />모자
+		<input type="checkbox" name="item" value="시계" />시계
+		<input type="checkbox" name="item" value="쥬얼리" />쥬얼리<br>
+		<input type="submit" value="전송" />
+	</form>
+</body>
+</html>
---
+++ 20180221 - web-study-02/src/unit09/CheckboxServlet.java
@@ -0,0 +1,39 @@
+package unit09;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CheckboxServlet")
+public class CheckboxServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>");
+		
+		String items[] = request.getParameterValues("item");
+		if(items == null) {
+			out.println("선택한 항목이 없습니다.");
+		}else {
+			out.println("당신이 선택한 항목입니다.<hr>");
+			for(String item : items) {
+				out.print(item + " ");
+			}
+		}
+		
+		out.println("<br><a href='javascript:history.go(-1)'>다시</a>");
+		out.print("</body></html>");
+		out.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
