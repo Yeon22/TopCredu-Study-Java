c129a6bdf9f9992e2187e7b5dd3dfa5e39c6dd20
Yeon <bsuyeon16@gamil.com>
2018-03-14 11:07:51 +0900

    JSP,Servlet 수업 - MVC2패턴:자바빈 클래스 생성 20180314

--- 20180314 - wet-study-11/src/com/saeyan/dto/BoardVO.java
+++ 20180314 - wet-study-11/src/com/saeyan/dto/BoardVO.java
@@ -1,5 +1,63 @@
 package com.saeyan.dto;
 
-public class BoardVO {
+import java.sql.Timestamp;
 
+public class BoardVO {
+	private int num;
+	private String pass;
+	private String name;
+	private String email;
+	private String title;
+	private String content;
+	private int readcount;
+	private Timestamp writedate;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getReadcount() {
+		return readcount;
+	}
+	public void setReadcount(int readcount) {
+		this.readcount = readcount;
+	}
+	public Timestamp getWritedate() {
+		return writedate;
+	}
+	public void setWritedate(Timestamp writedate) {
+		this.writedate = writedate;
+	}
 }
