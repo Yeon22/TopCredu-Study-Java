c4b215476942ea954ee46c5a3b01aa1f4f85e761
Yeon <bsuyeon16@gamil.com>
2018-03-15 13:48:35 +0900

    JSP,Servlet 수업 - 게시판 만들기:게시물 삭제 20180315

--- 20180314 - wet-study-11/src/com/saeyan/controller/action/BoardDeleteAction.java
+++ 20180314 - wet-study-11/src/com/saeyan/controller/action/BoardDeleteAction.java
@@ -6,12 +6,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.saeyan.dao.BoardDAO;
+
 public class BoardDeleteAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-
+		String num = request.getParameter("num");
+		BoardDAO bDao = BoardDAO.getInstance();
+		bDao.deleteBoard(num);
+		new BoardListAction().execute(request, response);
 	}
 
 }
