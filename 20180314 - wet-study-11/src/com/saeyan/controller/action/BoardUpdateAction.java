ea7a0f1a6b5ea8b3f0d714b697d76c935d4f1782
Yeon <bsuyeon16@gamil.com>
2018-03-15 13:48:57 +0900

    JSP,Servlet 수업 - 게시판 만들기:게시물 수정 20180315

--- 20180314 - wet-study-11/src/com/saeyan/controller/action/BoardUpdateAction.java
+++ 20180314 - wet-study-11/src/com/saeyan/controller/action/BoardUpdateAction.java
@@ -6,12 +6,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.saeyan.dao.BoardDAO;
+import com.saeyan.dto.BoardVO;
+
 public class BoardUpdateAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-
+		BoardVO bVo = new BoardVO();
+		bVo.setNum(Integer.parseInt(request.getParameter("num")));
+		bVo.setName(request.getParameter("name"));
+		bVo.setPass(request.getParameter("pass"));
+		bVo.setEmail(request.getParameter("email"));
+		bVo.setTitle(request.getParameter("title"));
+		bVo.setContent(request.getParameter("content"));
+		
+		BoardDAO bDao = BoardDAO.getInstance();
+		bDao.updateBoard(bVo);
+		new BoardListAction().execute(request, response);
 	}
 
 }
