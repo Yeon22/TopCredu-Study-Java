bdd673a576a5ffd5c67f021dfdafdbcf74c88730
Yeon <bsuyeon16@gamil.com>
2018-03-21 10:24:59 +0900

    쇼핑몰 만들기(구식) - DBManager 생성 20180321

---
+++ 20180320 - Portfolio/src/utill/DBManager.java
@@ -0,0 +1,49 @@
+package utill;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBManager {
+	//DBCP 연결 함수
+	public static Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource) envContext.lookup("jdbc/myoracle");
+			conn = ds.getConnection();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	//select 수행 후 리소스 해제를 위한 메소드
+	public static void close(Connection conn, Statement stmt, ResultSet rs) {
+		try {
+			rs.close();
+			stmt.close();
+			conn.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//insert, update, delete 작업을 수행한 후 리소스 해제를 위한 메소드
+	public static void close(Connection conn, Statement stmt) {
+		try {
+			stmt.close();
+			conn.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
